7f788c120ed634300b2a09c4d65557bceaacce0c
Ayush Bhosale <bhosaleayush45@gmail.com>
2026-07-07 23:49:40 +0530

    Remove debug logs

--- src/api/importApi.ts
+++ src/api/importApi.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const API = `${import.meta.env.VITE_API_URL.replace(/\/$/, "")}/api`;
 
-console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
-console.log("API =", API);
-
 export const importPlaylist=async(url:string)=>{
 
     const response=await axios.post(`${API}/import`,{
