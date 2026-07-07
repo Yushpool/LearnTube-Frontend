20a1a74ddd4e0409e51a8e4dee5ed6add5ce264e
Ayush Bhosale <bhosaleayush45@gmail.com>
2026-07-07 11:55:27 +0530

    Use environment variable for backend URL

--- src/api/lessonApi.ts
+++ src/api/lessonApi.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/api";
+const API = `${import.meta.env.VITE_API_URL}/api`;
+
 
 export const updateProgress = async (
     lessonId: number,
